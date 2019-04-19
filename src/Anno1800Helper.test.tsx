29f40aa97e3dcaf3144eba210a17e54bedfce03b
glkwhr <glkwhr@gmail.com>
2019-04-18 20:06:47 -0700

    Initial commit

--- src/Anno1800Helper.test.tsx
+++ src/Anno1800Helper.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Anno1800Helper from './Anno1800Helper';
+import { params } from './data/params_2019-04-17_full'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Anno1800Helper data={params}/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Anno1800Helper from './Anno1800Helper';
 import * as serviceWorker from './serviceWorker';
+import { params } from './data/params_2019-04-17_full'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Anno1800Helper data={params}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
