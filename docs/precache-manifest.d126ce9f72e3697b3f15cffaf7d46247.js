6d608bfff9eeb75b5c6df492b9c3b6266af0c50c
Haoran Wu <glkwhr@gmail.com>
2019-04-24 01:30:28 -0700

    Create project page

---
+++ docs/precache-manifest.d126ce9f72e3697b3f15cffaf7d46247.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ed9f40c237c430187ed",
+    "url": "/anno1800helper/static/js/runtime~main.326bd53b.js"
+  },
+  {
+    "revision": "ceceb1c8034ff5281407",
+    "url": "/anno1800helper/static/js/main.441bcf47.chunk.js"
+  },
+  {
+    "revision": "956aa1ab269b068f0725",
+    "url": "/anno1800helper/static/js/2.2e6b4f4a.chunk.js"
+  },
+  {
+    "revision": "ceceb1c8034ff5281407",
+    "url": "/anno1800helper/static/css/main.2cc3ebec.chunk.css"
+  },
+  {
+    "revision": "956aa1ab269b068f0725",
+    "url": "/anno1800helper/static/css/2.e464e0be.chunk.css"
+  },
+  {
+    "revision": "5ab18118e352792b68d7fe8ec975c000",
+    "url": "/anno1800helper/index.html"
+  }
+];
