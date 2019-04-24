c2b4908bb8efed1890dc23f15780fefdf7391321
glkwhr <glkwhr@gmail.com>
2019-04-24 01:59:38 -0700

    Updates

--- src/components/__tests__/Anno1800Helper.react.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Anno1800Helper from '../Anno1800Helper.react';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Anno1800Helper/>, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
--- src/redux/reducers/index.ts
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import factoryStates from "./factoryStates";
-import language from "./language";
-import populations from "./populations";
-
-export default combineReducers({language, populations, factoryStates});
