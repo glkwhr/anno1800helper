23a4df87a99905feb362a91500a24fa14d149f06
glkwhr <glkwhr@gmail.com>
2019-04-25 23:22:59 -0700

    Updates

--- docs/precache-manifest.bcc97661c92d5af61019d7ca1a6ab051.js
+++ docs/precache-manifest.bcc97661c92d5af61019d7ca1a6ab051.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/anno1800helper/static/js/runtime~main.326bd53b.js"
   },
   {
-    "revision": "5903b6b3013eb961b57d",
-    "url": "/anno1800helper/static/js/main.822edc0b.chunk.js"
+    "revision": "36d369b077da7f0771e8",
+    "url": "/anno1800helper/static/js/main.be4b4f64.chunk.js"
   },
   {
     "revision": "b2555e9572c5f462d4dc",
     "url": "/anno1800helper/static/js/2.0095f492.chunk.js"
   },
   {
-    "revision": "5903b6b3013eb961b57d",
+    "revision": "36d369b077da7f0771e8",
     "url": "/anno1800helper/static/css/main.e0740451.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/anno1800helper/static/css/2.e464e0be.chunk.css"
   },
   {
-    "revision": "4c69d544e87898ae7add6dd272898a1c",
+    "revision": "d2f72e707e884ee189cc0e2ee3a210d5",
     "url": "/anno1800helper/index.html"
   }
 ];
