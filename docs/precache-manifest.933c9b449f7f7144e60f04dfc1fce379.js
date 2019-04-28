0b70b193b4ea6f010f969f57d43ed1013e0bfbf0
glkwhr <glkwhr@gmail.com>
2019-04-27 18:53:29 -0700

    Updates

---
+++ docs/precache-manifest.933c9b449f7f7144e60f04dfc1fce379.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ed9f40c237c430187ed",
+    "url": "/anno1800helper/static/js/runtime~main.326bd53b.js"
+  },
+  {
+    "revision": "ecbba7d73ba828479258",
+    "url": "/anno1800helper/static/js/main.6166ccc0.chunk.js"
+  },
+  {
+    "revision": "7734ffd75d5c04cf747d",
+    "url": "/anno1800helper/static/js/2.39d2fb9f.chunk.js"
+  },
+  {
+    "revision": "ecbba7d73ba828479258",
+    "url": "/anno1800helper/static/css/main.f67f994f.chunk.css"
+  },
+  {
+    "revision": "7734ffd75d5c04cf747d",
+    "url": "/anno1800helper/static/css/2.e464e0be.chunk.css"
+  },
+  {
+    "revision": "cf847b797fa1905e1fcde72e568ab001",
+    "url": "/anno1800helper/index.html"
+  }
+];
