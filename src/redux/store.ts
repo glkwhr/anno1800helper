dc4654b39f2c36afb7e3f7b63faf30a260c3effe
glkwhr <glkwhr@gmail.com>
2019-06-05 23:24:41 -0700

    Write/hydrate Redux state to/from localStorage

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,15 @@
-import { createStore } from "redux";
+import {createStore} from "redux";
 import rootReducer from "./reducers";
 
-export default createStore(rootReducer);
+// To be used to hydrate state
+const persistedState = JSON.parse(localStorage.getItem('reduxState') || '{}');
+
+const store = createStore(rootReducer, persistedState);
+
+// Every time state changes, will be written to localStorage.
+// TODO: Find a more efficient way to do localStorage
+store.subscribe(() => {
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+});
+
+export default store;
